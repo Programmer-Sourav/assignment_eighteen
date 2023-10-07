b63b03511047d3b71995ce999467868aa62124f7
Sourav-nath <35804676+Programmer-Sourav@users.noreply.github.com>
2023-10-07 17:47:50 +0530

    fix: itemsList

--- src/pages/ItemsList.jsx
+++ src/pages/ItemsList.jsx
@@ -18,44 +18,12 @@ console.log("SubCat", foodSubCategory)
 const mapOverItemsList = itemsList.map((eachItem)=>(eachItem))
 console.log("123", mapOverItemsList)
 
-const mapOverItems = itemsList.map((eachItem)=>(eachItem.subCatInfo.foodInfo.productDetails.product.productName))
+const mapOverItems = itemsList.map((eachItem)=>(eachItem.subCatInfo.foodInfo.productDetails.product.productName)) //undefined
 console.log("222", mapOverItems)
 
 const name = mapOverItems.map((item)=>(item))
 console.log("224")
 
-const testItemList = [  {
-    "subCatInfo": {
-        "foodInfo": {
-            "productDetails": {
-                "product": "651fa0c7c41bed37cc3c1ba7"
-            },
-            "_id": "651fa0c7c41bed37cc3c1ba9",
-            "foodSCType": "Basmati Rice",
-            "scQuantity": 23,
-            "__v": 0
-        }
-    },
-    "_id": "651fa0c7c41bed37cc3c1bab",
-    "foodCategory": "Rice",
-    "__v": 0
-}, 
-{
-    "subCatInfo": {
-        "foodInfo": {
-            "productDetails": {
-                "product": "651fa0c7c41bed37cc3c1baf"
-            },
-            "_id": "651fa0c7c41bed37cc3c1bb1",
-            "foodSCType": "Basmati Rice",
-            "scQuantity": 23,
-            "__v": 0
-        }
-    },
-    "_id": "651fa0c7c41bed37cc3c1bb3",
-    "foodCategory": "Rice",
-    "__v": 0
-}]
 
 useEffect(()=>{dispatch(getItems())}, [])
 //  Print Category,
@@ -72,55 +40,30 @@ useEffect(()=>{dispatch(getItems())}, [])
         <div> 
 
             {
-                // foodCategory.map((fc)=>(
-                //     <div key={fc}>
-                //         <h2>{fc}</h2>
-                //         {
-                //             itemsList.map((item)=>(
-                //                 <div>
-                //                    { fc === item.foodCategory ? <h4>{item.subCatInfo.foodInfo.foodSCType}</h4>: ""}
-                //                 {/* {itemsList.map((item)=>(
-                //                     item.subCatInfo.foodInfo.foodSCType === fsc ?
-                //                      <h4>{fsc} </h4> : ""
-                                    
-                //               ))} */}
-                //               {itemsList.map((eachItem)=>(
-                //                 <div>
-                //                <p> {eachItem.subCatInfo.foodInfo.productDetails.product.brandInfo} </p>
-                //                 </div>
-                //                 ))}
-                              
-                //               </div>
-                //             ))
                         
-
-                //         }
-                        
-                //          </div>
-                // ))
 
                 foodCategory.map((fc)=>(
                     <div key={fc}>
                         <h2>{fc}</h2>
                         {
-                            testItemList.reduce((acc, currentItem)=>(
-                                <div>
-                                    {console.log("Visiting currentItem:", currentItem, fc)}
-                                   {fc === currentItem.foodCategory && currentItem.subCatInfo && !acc.includes(currentItem.subCatInfo.foodInfo.foodSCType) ? <h4>{[...acc, currentItem.subCatInfo.foodInfo.foodSCType]}</h4>: acc}
-                                {/* {itemsList.map((item)=>(
-                                    item.subCatInfo.foodInfo.foodSCType === fsc ?
-                                     <h4>{fsc} </h4> : ""
+                            itemsList.filter((currentItem)=>(fc === currentItem.foodCategory))
+                            .reduce((acc, currentItem)=>(
+                               
                                     
-                              ))} */}
+                                   currentItem.subCatInfo  && !acc.includes(currentItem.subCatInfo.foodInfo.foodSCType) ? [...acc, currentItem.subCatInfo.foodInfo.foodSCType]: acc
+                                
                               
-                              </div>
-                            ), [])
+                            ), []).map((foodSCType, index)=>(
+                                <h4>{foodSCType}</h4>
+                            ))
                         
 
                         }
                         
                          </div>
                    ))
+
+                  
             }
          
         </div>
